ce680851189ae04c1f4311695226dcad2b78e58e
zonixess <theaoudom2525@gmail.com>
2025-06-21 15:04:47 +0700

    modify favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,12 @@ import Footer from '../components/Footer'
 export const metadata = {
   title: 'King Alliance FC',
   description: 'Official website of King Alliance Football Club',
+  icons: {
+    icon: '/images/logo/logo.png',
+    // You can also add different sizes
+    // apple: '/apple-touch-icon.png',
+    // shortcut: '/favicon-16x16.png',
+  },
 }
 
 export default function RootLayout({ children }) {
